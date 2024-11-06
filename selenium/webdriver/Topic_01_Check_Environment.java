e5435040458c978ca2c1a3696e0a45369dc114a2
ThaoTran <tranthanhthao2712@gmail.com>
2024-11-06 22:09:04 +0700

    Update gitignore

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -7,26 +7,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Topic_01_Check_Environment {
-    WebDriver driver;
+  WebDriver driver;
 
-    @Test
-    public void TC_01_Run_On_Firefox() {
-        driver = new FirefoxDriver();
-        driver.get("https://www.facebook.com/");
-        driver.quit();
-    }
+  @Test
+  public void TC_01_Run_On_Firefox() {
+    driver = new FirefoxDriver();
+    driver.get("https://www.facebook.com/");
+    driver.quit();
+  }
 
-    @Test
-    public void TC_02_Run_On_Chrome() {
-        driver = new ChromeDriver();
-        driver.get("https://www.facebook.com/");
-        driver.quit();
-    }
+  @Test
+  public void TC_02_Run_On_Chrome() {
+    driver = new ChromeDriver();
+    driver.get("https://www.facebook.com/");
+    driver.quit();
+  }
 
-    @Test
-    public void TC_03_Run_On_Edge() {
-        driver = new EdgeDriver();
-        driver.get("https://www.facebook.com/");
-        driver.quit();
-    }
+  @Test
+  public void TC_03_Run_On_Edge() {
+    driver = new EdgeDriver();
+    driver.get("https://www.facebook.com/");
+    driver.quit();
+  }
+
+
 }
